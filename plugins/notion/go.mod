244285ca8d4f6c0345340fa3a5792f0eb9c9310b
julien040 <48369040+julien040@users.noreply.github.com>
2025-02-09 10:57:44 +0100

    🐛 Fix Notion plugin for issues #31, #26, and #29

--- plugins/notion/go.mod
+++ plugins/notion/go.mod
@@ -2,23 +2,26 @@ module github.com/julien040/anyquery/plugins/notion
 
 go 1.23.2
 
+// Replace the notionapi module with a fork
+replace github.com/jomei/notionapi v1.13.3 => github.com/julien040/notionapi-anyquery-fork v1.14.0
+
 require (
 	github.com/adrg/xdg v0.5.3
-	github.com/dgraph-io/badger/v4 v4.4.0
+	github.com/dgraph-io/badger/v4 v4.5.1
+	github.com/gomarkdown/markdown v0.0.0-20250202022148-4f606c78d442
 	github.com/hashicorp/go-retryablehttp v0.7.7
-	github.com/jomei/notionapi v1.13.2
-	github.com/julien040/anyquery v0.1.5
+	github.com/jomei/notionapi v1.13.3
+	github.com/julien040/anyquery v0.1.6
 )
 
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/dgraph-io/ristretto/v2 v2.0.0 // indirect
+	github.com/dgraph-io/ristretto/v2 v2.1.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/fatih/color v1.17.0 // indirect
-	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
-	github.com/google/flatbuffers v24.3.25+incompatible // indirect
+	github.com/google/flatbuffers v24.12.23+incompatible // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-hclog v1.6.3 // indirect
 	github.com/hashicorp/go-plugin v1.6.2 // indirect
@@ -29,10 +32,10 @@ require (
 	github.com/oklog/run v1.0.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	go.opencensus.io v0.24.0 // indirect
-	golang.org/x/net v0.31.0 // indirect
-	golang.org/x/sys v0.27.0 // indirect
-	golang.org/x/text v0.20.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
 	google.golang.org/grpc v1.66.2 // indirect
-	google.golang.org/protobuf v1.34.2 // indirect
+	google.golang.org/protobuf v1.36.3 // indirect
 )
--- plugins/notion/marshalling.go
+++ plugins/notion/marshalling.go
@@ -7,17 +7,186 @@ import (
 	"strings"
 	"time"
 
+	"github.com/gomarkdown/markdown/ast"
+	"github.com/gomarkdown/markdown/parser"
 	"github.com/jomei/notionapi"
 )
 
 // This file contains functions that convert Notion types to Go types and vice versa.
 
-func richTextToString(richText []notionapi.RichText) string {
-	var s string
-	for _, r := range richText {
-		s += r.PlainText
+func richTextToMarkdown(richText []notionapi.RichText) string {
+	var s strings.Builder
+	for _, v := range richText {
+		if v.Text != nil {
+			switch {
+			case v.Annotations.Bold:
+				s.WriteString("**")
+				s.WriteString(v.Text.Content)
+				s.WriteString("**")
+			case v.Annotations.Italic:
+				s.WriteString("*")
+				s.WriteString(v.Text.Content)
+				s.WriteString("*")
+			case v.Annotations.Code:
+				s.WriteString("`")
+				s.WriteString(v.Text.Content)
+				s.WriteString("`")
+			case v.Annotations.Strikethrough:
+				s.WriteString("~~")
+				s.WriteString(v.Text.Content)
+				s.WriteString("~~")
+			case v.Text.Link != nil:
+				s.WriteString("[")
+				s.WriteString(v.Text.Content)
+				s.WriteString("](")
+				s.WriteString(v.Text.Link.Url)
+				s.WriteString(")")
+			default:
+				s.WriteString(v.Text.Content)
+
+			}
+		}
 	}
-	return s
+
+	return s.String()
+
+}
+
+func markdownToRichText(markdownStr string) []notionapi.RichText {
+	parser := parser.NewWithExtensions(parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock)
+
+	md := []byte(markdownStr)
+	mdAST := parser.Parse(md)
+
+	log.Printf("mdASTChildren: %T %#v", mdAST.AsContainer().Children[0].GetChildren(), mdAST.AsContainer().Children[0].GetChildren())
+
+	var richText []notionapi.RichText
+
+	document := mdAST.AsContainer()
+	if document == nil {
+		log.Printf("markdownToRichText: unsupported type %T", mdAST)
+		return nil
+	}
+	paragraph, ok := document.Children[0].(*ast.Paragraph)
+	if !ok || paragraph == nil {
+		log.Printf("markdownToRichText: unsupported type %T", document.Children[0])
+		return nil
+	}
+
+	for i, node := range paragraph.GetChildren() {
+		log.Printf("node[%d]: %T %+v", i, node, node)
+		switch n := node.(type) {
+		case *ast.Text:
+			// If the text is longer than 2000 characters, split it into multiple text objects
+			for i := 0; i < len(n.Literal); i += 2000 {
+				end := i + 2000
+				if end > len(n.Literal) {
+					end = len(n.Literal)
+				}
+
+				richText = append(richText, notionapi.RichText{
+					Type: notionapi.ObjectTypeText,
+					Text: &notionapi.Text{
+						Content: string(n.Literal[i:end]),
+					},
+					Annotations: &notionapi.Annotations{
+						Bold:   false,
+						Italic: false,
+						Color:  notionapi.ColorDefault,
+					},
+					PlainText: string(n.Literal[i:end]),
+				})
+			}
+
+		case *ast.Emph:
+			// Extract the text from the children
+			text := strings.Builder{}
+			for _, child := range n.GetChildren() {
+				if textNode, ok := child.(*ast.Text); ok && textNode != nil {
+					text.WriteString(string(textNode.Literal))
+				}
+			}
+			richText = append(richText, notionapi.RichText{
+				Type: notionapi.ObjectTypeText,
+				Text: &notionapi.Text{
+					Content: text.String(),
+				},
+				Annotations: &notionapi.Annotations{
+					Bold:   false,
+					Italic: true,
+					Color:  notionapi.ColorDefault,
+				},
+				PlainText: text.String(),
+			})
+		case *ast.Strong:
+			// Extract the text from the children
+			text := strings.Builder{}
+			for _, child := range n.GetChildren() {
+				if textNode, ok := child.(*ast.Text); ok && textNode != nil {
+					text.WriteString(string(textNode.Literal))
+				}
+			}
+			richText = append(richText, notionapi.RichText{
+				Type: notionapi.ObjectTypeText,
+				Text: &notionapi.Text{
+					Content: text.String(),
+				},
+				Annotations: &notionapi.Annotations{
+					Bold:   true,
+					Italic: false,
+					Color:  notionapi.ColorDefault,
+				},
+				PlainText: text.String(),
+			})
+		case *ast.Link:
+			richText = append(richText, notionapi.RichText{
+				Type: notionapi.ObjectTypeText,
+				Text: &notionapi.Text{
+					Content: string(n.Content),
+					Link:    &notionapi.Link{Url: string(n.Destination)},
+				},
+				Annotations: &notionapi.Annotations{
+					Color: notionapi.ColorDefault,
+				},
+				PlainText: string(n.Destination),
+			})
+		case *ast.Code:
+			richText = append(richText, notionapi.RichText{
+				Type: notionapi.ObjectTypeText,
+				Text: &notionapi.Text{
+					Content: string(n.Literal),
+				},
+				Annotations: &notionapi.Annotations{
+					Color: notionapi.ColorDefault,
+					Code:  true,
+				},
+				PlainText: string(n.Literal),
+			})
+		case *ast.Del:
+			// Extract the text from the children
+			text := strings.Builder{}
+			for _, child := range n.GetChildren() {
+				if textNode, ok := child.(*ast.Text); ok && textNode != nil {
+					text.WriteString(string(textNode.Literal))
+				}
+			}
+			richText = append(richText, notionapi.RichText{
+				Type: notionapi.ObjectTypeText,
+				Text: &notionapi.Text{
+					Content: text.String(),
+				},
+				Annotations: &notionapi.Annotations{
+					Bold:          false,
+					Italic:        false,
+					Color:         notionapi.ColorDefault,
+					Strikethrough: true,
+				},
+				PlainText: text.String(),
+			})
+		}
+	}
+
+	return richText
 }
 
 // Convert a Notion property to a Go type.
@@ -31,17 +200,27 @@ func richTextToString(richText []notionapi.RichText) string {
 // Arrays are returned as their JSON representation in a string.
 // Boolean like checkboxes are returned as an int with 1 for true and 0 for false.
 func unmarshal(v notionapi.Property) interface{} {
+	log.Printf("unmarshal %T: %#v", v, v)
 	if v == nil {
 		return nil
 	}
 	switch value := v.(type) {
 	case *notionapi.TextProperty:
-		return richTextToString(value.Text)
+		return richTextToMarkdown(value.Text)
 	case *notionapi.NumberProperty:
+		if value == nil {
+			return nil
+		}
 		return value.Number
 	case *notionapi.SelectProperty:
+		if value == nil {
+			return nil
+		}
 		return value.Select.Name
 	case *notionapi.MultiSelectProperty:
+		if value == nil {
+			return nil
+		}
 		s := make([]string, len(value.MultiSelect))
 		for i, v := range value.MultiSelect {
 			s[i] = v.Name
@@ -53,12 +232,20 @@ func unmarshal(v notionapi.Property) interface{} {
 		}
 		return string(jsonArr)
 	case *notionapi.DateProperty:
+		if value == nil {
+			return nil
+		}
 		var valueTime string
 		if value.Date == nil {
 			return nil
 		}
 		if value.Date.Start != nil {
-			valueTime = time.Time(*value.Date.Start).Format(time.RFC3339)
+			// Notion date can be handled as datetime, or date only
+			if value.Date.DateOnly {
+				valueTime = time.Time(*value.Date.Start).Format("2006-01-02")
+			} else {
+				valueTime = time.Time(*value.Date.Start).Format("2006-01-02T15:04:05Z")
+			}
 		}
 
 		if value.Date.End != nil && value.Date.Start != nil {
@@ -66,17 +253,27 @@ func unmarshal(v notionapi.Property) interface{} {
 		}
 
 		if value.Date.End != nil {
-			valueTime += time.Time(*value.Date.End).Format(time.RFC3339)
+			if value.Date.DateOnly {
+				valueTime += time.Time(*value.Date.End).Format("2006-01-02")
+			} else {
+				valueTime += time.Time(*value.Date.End).Format("2006-01-02T15:04:05Z")
+			}
 		}
 
 		return valueTime
 	case *notionapi.CheckboxProperty:
+		if value == nil {
+			return nil
+		}
 		if value.Checkbox {
 			return 1
 		}
 		return 0
 
 	case *notionapi.RelationProperty:
+		if value == nil {
+			return nil
+		}
 		ids := make([]string, 0, len(value.Relation))
 		for _, v := range value.Relation {
 			ids = append(ids, v.ID.String())
@@ -89,8 +286,14 @@ func unmarshal(v notionapi.Property) interface{} {
 		}
 		return string(jsonArr)
 	case *notionapi.URLProperty:
+		if value == nil {
+			return nil
+		}
 		return value.URL
 	case *notionapi.FormulaProperty:
+		if value == nil {
+			return nil
+		}
 		switch value.Formula.Type {
 		case notionapi.FormulaTypeString:
 			return value.Formula.String
@@ -118,9 +321,12 @@ func unmarshal(v notionapi.Property) interface{} {
 			return nil
 		}
 	case *notionapi.PeopleProperty:
+		if value == nil {
+			return nil
+		}
 		ids := make([]string, 0, len(value.People))
 		for _, v := range value.People {
-			ids = append(ids, v.Name)
+			ids = append(ids, string(v.ID))
 		}
 		// Marshal the array to a string
 		jsonArr, err := json.Marshal(ids)
@@ -130,6 +336,9 @@ func unmarshal(v notionapi.Property) interface{} {
 		return string(jsonArr)
 
 	case *notionapi.FilesProperty:
+		if value == nil {
+			return nil
+		}
 		ids := make([]string, 0, len(value.Files))
 		for _, v := range value.Files {
 			if v.Type == notionapi.FileTypeExternal {
@@ -146,22 +355,49 @@ func unmarshal(v notionapi.Property) interface{} {
 		return string(jsonArr)
 
 	case *notionapi.PhoneNumberProperty:
+		if value == nil {
+			return nil
+		}
 		return value.PhoneNumber
 	case *notionapi.EmailProperty:
+		if value == nil {
+			return nil
+		}
 		return value.Email
 	case *notionapi.TitleProperty:
-		return richTextToString(value.Title)
+		if value == nil {
+			return nil
+		}
+		return richTextToMarkdown(value.Title)
 	case *notionapi.RichTextProperty:
-		return richTextToString(value.RichText)
+		if value == nil {
+			return nil
+		}
+		return richTextToMarkdown(value.RichText)
 	case *notionapi.CreatedByProperty:
+		if value == nil {
+			return nil
+		}
 		return value.CreatedBy.Name
 	case *notionapi.CreatedTimeProperty:
+		if value == nil {
+			return nil
+		}
 		return time.Time(value.CreatedTime).Format(time.RFC3339)
 	case *notionapi.LastEditedByProperty:
+		if value == nil {
+			return nil
+		}
 		return value.LastEditedBy.Name
 	case *notionapi.LastEditedTimeProperty:
+		if value == nil {
+			return nil
+		}
 		return time.Time(value.LastEditedTime).Format(time.RFC3339)
 	case *notionapi.RollupProperty:
+		if value == nil {
+			return nil
+		}
 		switch value.Rollup.Type {
 		case notionapi.RollupTypeNumber:
 			return value.Rollup.Number
@@ -183,8 +419,14 @@ func unmarshal(v notionapi.Property) interface{} {
 			return nil
 		}
 	case *notionapi.StatusProperty:
+		if value == nil {
+			return nil
+		}
 		return value.Status.Name
 	case *notionapi.UniqueIDProperty:
+		if value == nil {
+			return nil
+		}
 		id := strconv.Itoa(value.UniqueID.Number)
 		if value.UniqueID.Prefix != nil {
 			id = *value.UniqueID.Prefix + id
@@ -205,31 +447,10 @@ func marshal(value interface{}, to notionapi.PropertyConfig) notionapi.Property 
 	}
 	switch to.GetType() {
 	case notionapi.PropertyConfigTypeRichText, notionapi.PropertyConfigTypeTitle:
-		v := ""
-		if _, ok := value.(string); ok {
-			v = value.(string)
-		} else if _, ok := value.(float64); ok {
-			v = strconv.FormatFloat(value.(float64), 'f', -1, 64)
-		} else if _, ok := value.(int); ok {
-			v = strconv.Itoa(value.(int))
-		} else {
+		if _, ok := value.(string); !ok {
 			return nil
 		}
-		arrayRichText := []notionapi.RichText{
-			{
-				Type: notionapi.ObjectTypeText,
-				Text: &notionapi.Text{
-					Content: v,
-					Link:    nil,
-				},
-				Annotations: &notionapi.Annotations{
-					Bold:   false,
-					Italic: false,
-					Color:  notionapi.ColorDefault,
-				},
-				PlainText: v,
-			},
-		}
+		arrayRichText := markdownToRichText(value.(string))
 		if to.GetType() == notionapi.PropertyConfigTypeRichText {
 			return notionapi.RichTextProperty{
 				RichText: arrayRichText,
@@ -309,6 +530,7 @@ func marshal(value interface{}, to notionapi.PropertyConfig) notionapi.Property 
 		// Split the date into start and end
 		splitted := strings.Split(v, "/")
 		var start, end *notionapi.Date
+		dateOnly := false
 		if len(splitted) > 0 {
 			startParse, err := parseTime(splitted[0])
 			if err != nil {
@@ -316,6 +538,13 @@ func marshal(value interface{}, to notionapi.PropertyConfig) notionapi.Property 
 			}
 			val := notionapi.Date(startParse)
 			start = &val
+			// If the date is only 10 characters long (YYYY-MM-DD), it's a date only
+			// It's a workaround for the Notion library, which set the DateOnly field to true
+			// to indicate that the date is only a date, not a datetime
+			if len(splitted[0]) == 10 {
+				dateOnly = true
+			}
+
 		}
 		if len(splitted) > 1 {
 			endParse, err := parseTime(splitted[1])
@@ -324,12 +553,15 @@ func marshal(value interface{}, to notionapi.PropertyConfig) notionapi.Property 
 			}
 			val := notionapi.Date(endParse)
 			end = &val
+
+			dateOnly = dateOnly || len(splitted[1]) == 10
 		}
 
 		return notionapi.DateProperty{
 			Date: &notionapi.DateObject{
-				Start: start,
-				End:   end,
+				Start:    start,
+				End:      end,
+				DateOnly: len(splitted[0]) == 10,
 			},
 		}
 
@@ -390,6 +622,26 @@ func marshal(value interface{}, to notionapi.PropertyConfig) notionapi.Property 
 			},
 		}
 
+	case notionapi.PropertyConfigTypePeople:
+		// Get the IDs from the JSON array
+		v, ok := value.(string)
+		if !ok {
+			return nil
+		}
+		people := []string{}
+		err := json.Unmarshal([]byte(v), &people)
+		if err != nil {
+			return nil
+		}
+
+		peopleArray := []notionapi.User{}
+		for _, id := range people {
+			peopleArray = append(peopleArray, notionapi.User{ID: notionapi.UserID(id)})
+		}
+		return notionapi.PeopleProperty{
+			People: peopleArray,
+		}
+
 	// We don't support these types yet
 	// TODO
 	case notionapi.PropertyConfigTypeRelation:
@@ -398,8 +650,7 @@ func marshal(value interface{}, to notionapi.PropertyConfig) notionapi.Property 
 		return nil
 	case notionapi.PropertyConfigTypeRollup:
 		return nil
-	case notionapi.PropertyConfigTypePeople:
-		return nil
+
 	case notionapi.PropertyConfigTypeFiles:
 		return nil
 	}
