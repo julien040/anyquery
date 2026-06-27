b4d3ced5222367233bd89af9ce6b44a80a99e2a8
julien040 <48369040+julien040@users.noreply.github.com>
2026-06-27 21:27:09 +0200

    Upgrade Astro to latest version

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -9,6 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     site: "https://anyquery.dev",
+    compressHTML: true,
 
     integrations: [
         alpinejs(),
